0cf7fe720d4f704918bee7cf93a91a696ff09eb4
Adam Koeninger <akoeninger21@gmail.com>
2017-03-06 13:32:48 -0600

    Chapter 3 exercises

--- exercises/chapter3/src/Data/AddressBook.purs
+++ exercises/chapter3/src/Data/AddressBook.purs
@@ -3,7 +3,7 @@ module Data.AddressBook where
 import Prelude
 
 import Control.Plus (empty)
-import Data.List (List(..), filter, head)
+import Data.List (List(..), filter, head, nubBy, null)
 import Data.Maybe (Maybe)
 
 type Address =
@@ -24,7 +24,9 @@ showAddress :: Address -> String
 showAddress addr = addr.street <> ", " <> addr.city <> ", " <> addr.state
 
 showEntry :: Entry -> String
-showEntry entry = entry.lastName <> ", " <> entry.firstName <> ": " <> showAddress entry.address
+showEntry entry = entry.lastName <> ", " <>
+                  entry.firstName <> ": " <>
+                  showAddress entry.address
 
 emptyBook :: AddressBook
 emptyBook = empty
@@ -37,3 +39,22 @@ findEntry firstName lastName = head <<< filter filterEntry
   where
   filterEntry :: Entry -> Boolean
   filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
+
+findByStreet :: String -> AddressBook -> Maybe Entry
+findByStreet street = head <<< filter filterEntry
+  where
+  filterEntry :: Entry -> Boolean
+  filterEntry entry = entry.address.street == street
+
+hasEntry :: String -> String -> AddressBook -> Boolean
+hasEntry firstName lastName = not <<< null <<< filter filterEntry
+  where
+  filterEntry :: Entry -> Boolean
+  filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
+
+removeDuplicates :: AddressBook -> AddressBook
+removeDuplicates book = nubBy entryEquality book
+  where
+  entryEquality :: Entry -> Entry -> Boolean
+  entryEquality e1 e2 = e1.firstName == e2.firstName && e1.lastName == e2.lastName
+
