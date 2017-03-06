0305ff385b77cd7f30d114cdca378ed0835cbc9d
Adam Koeninger <akoeninger21@gmail.com>
2017-03-06 14:06:17 -0600

    Chapter4 Exercises
    * 4.4 Recursion on Arrays problem 1

--- exercises/chapter4/src/FileOperations.purs
+++ exercises/chapter4/src/FileOperations.purs
@@ -5,6 +5,20 @@ import Prelude
 import Data.Path (Path, ls)
 import Data.Array (concatMap, (:))
 
+fact :: Int -> Int
+fact 0 = 1
+fact n = n * fact (n - 1)
+
+fib :: Int -> Int
+fib 0 = 1
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+evenInt :: Int -> Boolean
+evenInt 0 = true
+evenInt 1 = false
+evenInt n = evenInt (n - 2)
+
 allFiles :: Path -> Array Path
 allFiles root = root : concatMap allFiles (ls root)
 
