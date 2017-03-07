15b787c55454886d0ea4d2a9a047d5c208f5b633
Adam Koeninger <akoeninger21@gmail.com>
2017-03-07 11:21:49 -0600

    Exercise 2 Chapter 4.4 Recursion on Arrays
    * Wonder what the idiomatic way to do nested ifs is

--- exercises/chapter4/src/FileOperations.purs
+++ exercises/chapter4/src/FileOperations.purs
@@ -3,7 +3,9 @@ module FileOperations where
 import Prelude
 
 import Data.Path (Path, ls)
-import Data.Array (concatMap, (:))
+import Data.Array (concatMap, (:), null)
+import Data.Array.Partial (head, tail)
+import Partial.Unsafe (unsafePartial)
 
 fact :: Int -> Int
 fact 0 = 1
@@ -26,3 +28,13 @@ allFiles' :: Path -> Array Path
 allFiles' file = file : do
   child <- ls file
   allFiles' child
+
+countEvens :: Array Int -> Int
+countEvens arr =
+  if null arr
+    then 0
+    else
+      countEvens (unsafePartial tail arr) + if evenInt (unsafePartial head arr)
+        then 1
+        else 0
+
