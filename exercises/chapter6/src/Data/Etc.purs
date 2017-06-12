b054c9e1e45ede7451815232c661c8111db3f142
Adam Koeninger <akoeninger21@gmail.com>
2017-06-12 08:41:41 -0500

    More data type exercies

--- exercises/chapter6/src/Data/Etc.purs
+++ exercises/chapter6/src/Data/Etc.purs
@@ -1,7 +1,6 @@
 module Data.Etc where
 
 import Prelude
-
 import Data.Foldable
 
 -- 6.4 Show and Eq instances for Complex
@@ -13,4 +12,43 @@ instance showComplex :: Show Complex where
 
 instance eqComplex :: Eq Complex where
   eq (Complex c1) (Complex c2) = c1.real == c2.real && c1.imaginary == c2.imaginary
+-- 6.5-7 Type Annotations
+
+data NonEmpty a = NonEmpty a (Array a)
+
+instance eqNonEmpty :: Eq a => Eq (NonEmpty a) where
+  eq (NonEmpty x xs) (NonEmpty y ys) = x == y && xs == ys
+
+
+instance semigroupNonEmpty :: Semigroup a => Semigroup (NonEmpty a) where
+  append (NonEmpty x xs) (NonEmpty y ys) = NonEmpty x (xs <> [y] <> ys)
+
+instance functorNonEmpty :: Functor NonEmpty where
+  map f (NonEmpty x xs) = NonEmpty (f x) (f <$> xs)
+
+instance foldableNonEmpty :: Foldable NonEmpty where
+  foldr f z (NonEmpty x xs) = foldr f z ([x] <> xs)
+  foldl f z (NonEmpty x xs) = foldl f z ([x] <> xs)
+  foldMap f (NonEmpty x xs) = foldMap f ([x] <> xs)
+
+data Extended a = Finite a | Infinite
+
+instance eqExtended :: Eq a => Eq (Extended a) where
+  eq Infinite Infinite = true
+  eq Infinite _ = false
+  eq _ Infinite = false
+  eq (Finite x) (Finite y) = eq x y
+
+instance ordExtended :: Ord a => Ord (Extended a) where
+  compare Infinite Infinite = EQ
+  compare Infinite _ = GT
+  compare _ Infinite = LT
+  compare (Finite x) (Finite y) = compare x y
+
+data OneMore f a = OneMore a (f a)
+
+instance foldableOneMore :: Foldable f => Foldable (OneMore f) where
+  foldr f z (OneMore x ys) = f x (foldr f z ys)
+  foldl f z (OneMore x ys) = f (foldl f z ys) x
+  foldMap f (OneMore x ys) = (f x) <> (foldMap f ys)
 
