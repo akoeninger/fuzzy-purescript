84f984b9b0facf6ae86ab0b568646d3b2aa81c19
Adam Koeninger <akoeninger21@gmail.com>
2017-06-13 14:05:07 -0500

    Exercise 7.10 Validation

--- exercises/chapter7/src/Data/AddressBook/Validation.purs
+++ exercises/chapter7/src/Data/AddressBook/Validation.purs
@@ -30,15 +30,27 @@ phoneNumberRegex =
     case regex "^\\d{3}-\\d{3}-\\d{4}$" noFlags of
       Right r -> r
 
+stateRegex :: Regex
+stateRegex =
+  unsafePartial
+    case regex "^[a-z]{2}|[A-Z]{2}$" noFlags of
+      Right r -> r
+
+nonWhitespaceRegex :: Regex
+nonWhitespaceRegex =
+  unsafePartial
+    case regex "^\\S++$" noFlags of
+      Right r -> r
+
 matches :: String -> Regex -> String -> V Errors Unit
 matches _     regex value | test regex value = pure unit
 matches field _     _     = invalid ["Field '" <> field <> "' did not match the required format"]
 
 validateAddress :: Address -> V Errors Address
 validateAddress (Address o) =
-  address <$> (nonEmpty "Street" o.street *> pure o.street)
-          <*> (nonEmpty "City"   o.city   *> pure o.city)
-          <*> (lengthIs "State" 2 o.state *> pure o.state)
+  address <$> (nonWhitespace "Street" o.street *> pure o.street)
+          <*> (nonWhitespace "City"   o.city   *> pure o.city)
+          <*> (validateState o.state)
 
 validatePhoneNumber :: PhoneNumber -> V Errors PhoneNumber
 validatePhoneNumber (PhoneNumber o) =
@@ -47,10 +59,21 @@ validatePhoneNumber (PhoneNumber o) =
 
 validatePerson :: Person -> V Errors Person
 validatePerson (Person o) =
-  person <$> (nonEmpty "First Name" o.firstName *> pure o.firstName)
-         <*> (nonEmpty "Last Name"  o.lastName  *> pure o.lastName)
+  person <$> (nonWhitespace "First Name" o.firstName *> pure o.firstName)
+         <*> (nonWhitespace "Last Name"  o.lastName  *> pure o.lastName)
          <*> validateAddress o.homeAddress
          <*> (arrayNonEmpty "Phone Numbers" o.phones *> traverse validatePhoneNumber o.phones)
 
 validatePerson' :: Person -> Either Errors Person
 validatePerson' p = unV Left Right $ validatePerson p
+
+validateState :: String -> V Errors String
+validateState state = 
+  lengthIs "State" 2 state *> matches "State" stateRegex state *> pure state
+
+nonWhitespace :: String -> String -> V Errors Unit
+nonWhitespace _ str | test nonWhitespaceRegex str = pure unit
+nonWhitespace field _ = invalid ["Field '" <> field <> "' cannot be whitespace"]
+
+
+
