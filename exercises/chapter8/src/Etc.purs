34f64dc744c2dc45367b73d282d19aa7a4122da4
Adam Koeninger <akoeninger21@gmail.com>
2017-06-26 10:38:03 -0500

    Wrote sums function which uses foldM from Data.Array

--- exercises/chapter8/src/Etc.purs
+++ exercises/chapter8/src/Etc.purs
@@ -2,11 +2,14 @@ module Etc where
 
 import Prelude
 import Data.Array
-import Data.Maybe
+import Data.Maybe (Maybe)
 
 third :: forall a. Array a -> Maybe a
 third a = do
   first <- tail a
   second <- tail first
   head second
+
+sums :: Array Int -> Array Int
+sums = sort <<< nub <<< foldM (\x y -> [x, x + y]) 0
 
