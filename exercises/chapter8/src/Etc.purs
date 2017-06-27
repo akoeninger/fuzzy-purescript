87c4af5c1513fc67070cecf8907836ae85d4aa71
Adam Koeninger <akoeninger21@gmail.com>
2017-06-27 15:04:15 -0500

    Wrote filterM method

--- exercises/chapter8/src/Etc.purs
+++ exercises/chapter8/src/Etc.purs
@@ -1,7 +1,11 @@
 module Etc where
 
 import Prelude
+import Control.Monad
+import Control.MonadPlus
+import Control.MonadZero
 import Data.Array
+import Data.List (List(..), (:))
 import Data.Maybe (Maybe)
 
 third :: forall a. Array a -> Maybe a
@@ -12,4 +16,11 @@ third a = do
 
 sums :: Array Int -> Array Int
 sums = sort <<< nub <<< foldM (\x y -> [x, x + y]) 0
+
+filterM :: forall m a. Monad m => (a -> m Boolean) -> List a -> m (List a)
+filterM _ Nil = pure Nil
+filterM f (a : as) = do
+  keep <- f a
+  xs <- filterM f as
+  pure if keep then a : xs else xs
 
